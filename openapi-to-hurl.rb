57d529183ef1f7b930405d78fd109607b19df853
ethancarlsson <ethanmcarlsson@gmail.com>
2024-02-24 21:01:04 +0100

    fix: broken ruby file

--- openapi-to-hurl.rb
+++ openapi-to-hurl.rb
@@ -6,6 +6,7 @@ class OpenapiToHurlBin < Formula
   if OS.mac?
     url "https://github.com/ethancarlsson/openapi-to-hurl/releases/download/#{version}/openapi-to-hurl-1.0.0-x86_64-apple-darwin.tar.gz"
       sha256 "2e4631394886f39bf68c1f0e0f2163dcb7fd00895a495d6e7064735153019b78"
+  end
 
   conflicts_with "openapi-to-hurl"
 

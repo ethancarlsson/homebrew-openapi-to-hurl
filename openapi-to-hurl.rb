7735f6864460a8c815e31fe00e3bbe315530dda6
ethancarlsson <ethanmcarlsson@gmail.com>
2024-02-25 10:16:22 +0100

    fix: Update sha to match x86_64 release

--- openapi-to-hurl.rb
+++ openapi-to-hurl.rb
@@ -5,7 +5,7 @@ class OpenapiToHurl < Formula
 
   if OS.mac?
     url "https://github.com/ethancarlsson/openapi-to-hurl/releases/download/#{version}/openapi-to-hurl-1.0.0-x86_64-apple-darwin.tar.gz"
-      sha256 "2e4631394886f39bf68c1f0e0f2163dcb7fd00895a495d6e7064735153019b78"
+      sha256 "adea9bbfa36f7c6fe8364aba3aae0f6286d97b983ea8e84c695ae014a85d8b74"
   end
 
   conflicts_with "openapi-to-hurl"

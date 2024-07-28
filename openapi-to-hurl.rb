32069af2c39e73c007c5c5d3b860c6380ebebc2b
ethancarlsson <ethanmcarlsson@gmail.com>
2024-07-28 11:26:32 +0200

    feat: Bump version to v1.2.0

--- openapi-to-hurl.rb
+++ openapi-to-hurl.rb
@@ -1,15 +1,15 @@
 class OpenapiToHurl < Formula
-  version '1.1.0'
+  version '1.2.0'
   desc "Turn an openapi specification into hurl files"
   homepage "https://github.com/ethancarlsson/openapi-to-hurl"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/ethancarlsson/openapi-to-hurl/releases/download/v#{version}/openapi-to-hurl-#{version}-arm64.tar.gz"
-        sha256 "47fce4808458a07bc47ac2ea8182e0dac5745925510c25ae3063bbc04c90ffaa"
+        sha256 "68e05d870060e6856918e9147118abd4714ed8057f8eb4ebe60ec986c80afb89"
     elsif Hardware::CPU.intel?
       url "https://github.com/ethancarlsson/openapi-to-hurl/releases/download/v#{version}/openapi-to-hurl-#{version}-x86_64-apple-darwin.tar.gz"
-        sha256 "460487218bf69737e6d7a5f82dfeb28df1718745ddc3b030aeed8850f2e9ec2a"
+        sha256 "d7b8af8dfb17ad4cb9d3f6a6d3303ad15a43a6f640e520c7c0fb13e8b4163145"
     end
   end
 
